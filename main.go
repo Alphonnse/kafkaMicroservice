28bdb635c1b5bdefe6c119e40d5f9570c825cfc4
Alphonnse <nderzyan@gmail.com>
2024-03-05 23:12:46 +0300

    git init

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+type OrderPlacer struct {
+	producer   *kafka.Producer
+	topic      string
+	deliverych chan kafka.Event
+}
+
+func NewOrderProducer(p *kafka.Producer, topic string) *OrderPlacer {
+	return &OrderPlacer{
+		producer:   p,
+		topic:      topic,
+		deliverych: make(chan kafka.Event, 10000),
+	}
+}
+
+func (op *OrderPlacer) placeOrder(orderType string, size int) error {
+	var (
+		format  = fmt.Sprintf("%s - %d", orderType, size)
+		payload = []byte(format)
+	)
+
+	err := op.producer.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{
+			Topic:     &op.topic,
+			Partition: kafka.PartitionAny,
+		},
+		Value: payload, // Best way to do this is a protobuf
+	},
+		op.deliverych,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	<-op.deliverych
+
+	fmt.Printf("Placed order on the queue: %s\n", format)
+
+	return nil
+}
+
+func main() {
+	// Producer
+	p, err := kafka.NewProducer(&kafka.ConfigMap{
+		"bootstrap.servers": "localhost:9092",
+		"client.id":         "something",
+		"acks":              "all",
+	})
+	if err != nil {
+		fmt.Printf("Failed to create producer: %s\n", err)
+	}
+
+	op := NewOrderProducer(p, "HVSE")
+
+	for i:=0;i<1000;i++{
+		if err := op.placeOrder("maret", i+1); err != nil {
+			log.Fatal(err)
+		}
+		time.Sleep(time.Second*3)
+	}
+}
---
+++ processor/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+// There also can be different froup IDs to read same mesages not once
+func main() {
+	// Processor
+	topic := "HVSE"
+	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": "localhost:9092",
+		"group.id":          "foo",
+		"auto.offset.reset": "smallest",
+	})
+	defer consumer.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = consumer.Subscribe(topic, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		consumer.Close()
+		os.Exit(1)
+	}()
+
+	for {
+		ev := consumer.Poll(100)
+		switch e := ev.(type) {
+		case *kafka.Message:
+			fmt.Printf("Processing order: %s\n", string(e.Value))
+		case *kafka.Error:
+			fmt.Printf("%v\n", e)
+		}
+	}
+}
